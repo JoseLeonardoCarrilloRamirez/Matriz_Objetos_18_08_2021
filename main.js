5e8ad7182f0b55732329eb36ebe87f4f8f8f4807
JoseLeonardoCarrilloRamirez <joseleonardocarrillo@gmail.com>
2021-08-30 20:02:23 -0500

    Actualizacion 20210830

--- main.js
+++ main.js
@@ -26,22 +26,22 @@
 
 
 
-// let D = 5;
-// let matriz = new Array(D).fill(null);
+ let D = 5;
+ let matriz = new Array(D).fill(null);
 
-// for(var i=0; i<matriz.length; i++) {
-//     matriz[i] = new Array(D).fill(null);
-//     for(var j=0; j<matriz[i].length; j++) {
-//         matriz[i][j] = new Array(D).fill(null);
-//         for(var r=0; r<matriz[i][j].length; r++) {
-//             matriz[i][j][r] = new Array(D).fill(null);
-//             for(var q=0; q<matriz[i][j][r].length; q++) {
-//                 matriz[i][j][r][q] = new Array(D).fill(null);
-//             }
-//         }
-//     }
-// }
-// let obj = {datos:matriz};
-// Object.preventExtensions(obj);
-// obj.datos[3][2][4][0][0] = "Miguel";
-// console.log(obj);
+ for(var i=0; i<matriz.length; i++) {
+     matriz[i] = new Array(D).fill(null);
+     for(var j=0; j<matriz[i].length; j++) {
+         matriz[i][j] = new Array(D).fill(null);
+         for(var r=0; r<matriz[i][j].length; r++) {
+             matriz[i][j][r] = new Array(D).fill(null);
+             for(var q=0; q<matriz[i][j][r].length; q++) {
+                 matriz[i][j][r][q] = new Array(D).fill(null);
+             }
+         }
+     }
+ }
+ let obj = {datos:matriz};
+ Object.preventExtensions(obj);
+ obj.datos[3][2][4][0][0] = "Jose";
+ console.log(obj);
