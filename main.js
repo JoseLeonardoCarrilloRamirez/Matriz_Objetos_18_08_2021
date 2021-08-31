86df76ca1f4e875b307a4a32fc5323413dfd95f8
JoseLeonardoCarrilloRamirez <joseleonardocarrillo@gmail.com>
2021-08-30 19:48:16 -0500

    Modificacion Comentarios

--- main.js
+++ main.js
@@ -3,19 +3,19 @@
 
 
 
-// let obj = {
-//     suma : function(){
-//         return this.num1 + this.num2;
-//     }
-// }
-// let obj2 = Object.create(obj);
-// obj2.num1 = 5;
-// obj2.num2 = 5;
+{ let obj = {
+     suma : function(){
+         return this.num1 + this.num2;
+     }
+ }
+ let obj2 = Object.create(obj);
+ obj2.num1 = 8;
+ obj2.num2 = 3;
 
-// console.log(obj2);
-// // console.log(obj2.suma());
+ console.log(obj2);
+ console.log(obj2.suma());
 
-
+}
 
 
 
